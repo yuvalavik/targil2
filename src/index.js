80611c6bff5fd2a847126b16bf38e501cd2923ff
EladSoffer <118102820+EladSoffer@users.noreply.github.com>
2023-05-08 18:37:03 +0300

    login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Register from './register/Register';
+import Login from './login/Login';
 
 const App = () => {
   const [user, setUser] = useState([]); // Initialize user state as null
 
   return (
     <React.StrictMode>
-      <Register setUser={setUser} user={user} />
+      <Router>
+    {/* <Route exact path="/" component={Register setUser={setUser} user={user}} /> */}
+        <Route path="/" component={Login} />
+        <Route path="/Register" render={() => <Register setUser={setUser} user={user}/>} />  
+        <Register setUser={setUser} user={user} />
+      </Router>
     </React.StrictMode>
   );
 };
