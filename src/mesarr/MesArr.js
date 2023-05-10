5a9ecced78f061adb298abcf6ea45cd6598b99ff
EladSoffer <118102820+EladSoffer@users.noreply.github.com>
2023-05-10 11:02:46 +0300

    send btn

--- src/mesarr/MesArr.js
+++ src/mesarr/MesArr.js
@@ -1,33 +1,30 @@
 import Message from "../message/Message";
+import React, { useRef, useEffect } from 'react';
 
-function MesArr(curuser) {
 
-    const messages = curuser.messages;
-    const messageComponents = [];
-    if (!curuser.messages) {
-        console.log(curuser.messages);
-        console.log("ff");
-        return null;
-        
-      }
-  
-    for (let i = 0; i < curuser.messages.length; i++) {
-      const message = messages[i];
-      messageComponents.push(
-        <Message
-          key={i}
-          side="user-ms ml-84"
-          content={message.value}
-          color="rgba(26, 6, 241, 0.575)"
-        />
-      );
-    }
-  
-    return (
-      <>
-        {messageComponents}
-    </>
-    );
+function MesArr({ curuser }) {
+    // const mesRef = useRef(null)
+    // useEffect (()=>{
+    //     mesRef.current.scrollTop = mesRef.current.scrollHeight
+
+    // },[curuser.messages])
+    console.log(curuser.messages);
+  if (!curuser || !curuser.messages || curuser.messages.length === 0) {
+    console.log(curuser);
+    console.log("ff");
+    return null;
   }
+
+  const messageComponents = curuser.messages.map((message, index) => (
+    <Message
+      key={index}
+      side="user-ms ml-84"
+      content={message}
+      color="rgba(26, 6, 241, 0.575)"
+    />
+  ));
+
+  return <>{messageComponents}</>;
+}
 
 export default MesArr;
